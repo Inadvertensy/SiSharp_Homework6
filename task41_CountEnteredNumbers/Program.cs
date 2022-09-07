2bdcc7c151a465d83b2300ccd52a57b067b89059
1 1 <bobra2@ya.ru>
2022-09-07 09:38:22 +0300

    tasks 41 and 43 added

---
+++ task41_CountEnteredNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры М чисел. 
+// Посчитайте, сколько чисел больше 0 ввел пользователь.
+// 0, 7, 8, -2, -2 ->2
+// 1, -7, 567, 89, 223 -> 3
+
+Console.WriteLine("Please enter the amount of numbers you want to enter");
+int M=Convert.ToInt32(Console.ReadLine());
+int [] array = getArray();
+Console.WriteLine("Array is "+String.Join(" ", array));
+int [] getArray( )
+{
+    int [] res=new int [M];
+    
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine($"Please enter the {i+1} number");
+        int num=Convert.ToInt32(Console.ReadLine());
+        res[i]=num;
+    }
+    return res;
+   
+}
+
+void countPosNumbers(int [] array)
+{
+    int count=0;
+    for (int j = 0; j < array.Length; j++)
+    {
+     if (array[j]>0) count ++;
+    }
+    Console.WriteLine($"Amount of positive numbers is:\t{count}");
+}
+ countPosNumbers(array);
---
+++ task43_TwoLineCrossing/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void TwolinesCrossing(){
+    
+Console.WriteLine($"Enter b1:\t");
+double b1=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Enter k1:\t");
+double k1=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Enter b2:\t");
+double b2=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Enter k2:\t");
+double k2=Convert.ToDouble(Console.ReadLine());
+
+double pX=(b2-b1)/(k1-k2);
+double pY=k1*((b2-b1)/(k1-k2))+b1;
+
+Console.WriteLine($"Coordinates of two line crossing are ({pX:f3};{pY:f3})");
+}
+TwolinesCrossing();
